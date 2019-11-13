8c300d4893d95d42c870ea0d65f00b2b98068f0d
Hansell Baran <hansell.baran@netlight.com>
2019-11-13 19:54:03 +0100

    Added navigation bar component - NavBar component used in all pages - Added node-sass & sass-loader to the project - Removed unnecessary "_.git" folder

---
+++ components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mb-5">
+    <b-navbar
+      variant="dark"
+      type="dark"
+      toggleable="lg"
+    >
+      <b-navbar-toggle target="nav-collapse" />
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <NuxtLink to="/">
+              Home
+            </NuxtLink>
+          </b-nav-item>
+          <b-nav-item>
+            <NuxtLink to="/movies">
+              Movies
+            </NuxtLink>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+.nav-item {
+  a {
+    color: whitesmoke;
+
+    &:hover {
+      color: orange;
+    }
+  }
+}
+</style>
